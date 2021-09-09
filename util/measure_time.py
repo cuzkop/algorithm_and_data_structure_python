749aa541f3c09783761ddd1089cf893e93978b16
tetsu <eiostu.m@gmail.com>
2021-09-09 17:26:16 +0900

    時間測定のためのデコレータ追加

---
+++ util/measure_time.py
@@ -0,0 +1,19 @@
+import time
+
+def measure(func):
+    def inner(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time()
+        print(f"{(t2 - t1):.4f}s")
+        return result
+    return inner
+
+# def snooze():
+#     time.sleep(1)
+
+# @time_decorator
+# def naptime():
+#     snooze()
+
+# naptime()
